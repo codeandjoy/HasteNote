a8c9f943b2b73f9f7b28c9d11223bb772b7bf093
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-09-02 11:51:48 +0300

    Minor change

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -4,8 +4,6 @@ import './css/Icon.css';
 const Icon = ({ type }) => {
     const classes = "icon icon-"+type;
 
-    console.log(classes);
-
     return (
         <div className={ classes }></div>
     )
