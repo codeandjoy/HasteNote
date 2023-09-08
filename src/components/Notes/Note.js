ba51cbbaa1a33bd2ddf6803de885ff2682ea0236
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-09-08 05:43:27 +0300

    Minor change

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -10,8 +10,7 @@ import "./css/Note.css";
 const Note = ({ note }) => {
     const setPageFadeActive = useSetRecoilState(pageFadeActive);
     const setPageFadeCallback = useSetRecoilState(pageFadeCallback);
-    const [editModalOpen, setEditModalOpen] = useState(true);
-    setPageFadeActive(true);
+    const [editModalOpen, setEditModalOpen] = useState(false);
     
     const [pos, setPos] = useState({x:0, y:0});
     const noteRef = useRef();
