0dd5a4f74810a72de2c8eef29440cb587df9644f
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-09-23 00:19:24 +0300

    Created MDNoteModal headeer, added inputs for other MDNote data

--- src/components/MDNoteModal/MDNoteModal.js
+++ src/components/MDNoteModal/MDNoteModal.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
+import PlainBtn from "../PlainBtn/PlainBtn";
+import Icon from "../Icon/Icon";
 
 import "./css/MDNoteModal.css";
 
@@ -7,15 +9,41 @@ import "./css/MDNoteModal.css";
 const MDNoteModal = () => {
     const [content, setContent] = useState("");
     
-    console.log(content);
+    const [formatVisible, setFormatVisible] = useState(false);
 
     return (
         <div className="MDNoteModal" data-color-mode="dark">
-            <MDEditor
-                value={ content }
-                onChange={ setContent }
-                className="MDEditor"
-            />
+            <div className="modal-header">
+                <PlainBtn
+                    type="menu-arrow-left"
+                    className="btn-back"
+                />
+                <Icon type="paper" className="art-paper"/>
+                <PlainBtn
+                    type={ formatVisible ? "hide" : "format" }
+                    className="btn-format"
+                    onClick={() => setFormatVisible(fv => !fv)}
+                />
+            </div>
+
+            <div className="note-header">
+                <input
+                    className="note-name"
+                    value="Markdown note"
+                />
+                <input
+                    className="note-tags"
+                    value="#work #school"
+                />
+            </div>
+
+            <div className="content-editor">
+                <MDEditor
+                    value={ content }
+                    onChange={ setContent }
+                    className={"MDEditor"+(formatVisible ? " format-visible":"")}
+                />
+            </div>
         </div>
     );
 }
