35169fc8b291716e404274ed845fd88708a427f4
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-09-24 10:22:37 +0300

    Added animations to MDNote

--- src/components/Notes/MDNote.js
+++ src/components/Notes/MDNote.js
@@ -1,13 +1,25 @@
+import { motion } from "framer-motion";
 import Icon from "../Icon/Icon";
 
 import "./css/NotePreview.css"
 import "./css/MDNote.css"
+import { noteVariants } from "./animationVariants";
 
 
 const MDNote = ({ note }) => {
     return (
-        <div
+        <motion.div
+            variants={ noteVariants }
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            layout
+
             className="note-preview md-note"
+
+            onClick={() => {
+                // Open MD modal
+            }}
         >
             <div className="md-note--art-side md-note--art-side-left"><Icon type="paper-black"/></div>
             <div className="md-note--art-side md-note--art-side-right"><Icon type="paper-black"/></div>
@@ -17,7 +29,7 @@ const MDNote = ({ note }) => {
                 <h3 className="note-title">{ note.title }</h3>
                 <div className="note-tags"><span>{ note.tags }</span></div>
             </div>
-        </div>
+        </motion.div>
     );
 };
 
